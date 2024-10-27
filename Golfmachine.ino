d8cd9db56187e927a209ce601615963f9d46736c
owiedepowie <101893046+owiedepowie@users.noreply.github.com>
2024-10-27 19:44:51 +0100

    Add files via upload

---
+++ Golfmachine.ino
@@ -0,0 +1,61 @@
+#include "gm.h"
+
+
+// pins
+#define dirPin 2
+#define stepPin 3
+#define knopPin1 5
+#define knopPin2 6
+#define knopPin3 7
+
+const long stepsPerRevolution = 7200; // aantal stappen per rondje, liefst deelbaar door 360 voor geen afrondingsproblemen
+// standaard instellingen voor modi
+const int draaiHoek1 = 30; // hoek in graden; 0 < draaiHoek <= 90; modus 1
+const int draaiHoek2 = 45; // hoek in graden; 0 < draaiHoek <= 90; modus 2
+const int draaiHoek3 = 90; // hoek in graden; 0 < draaiHoek <= 90; modus 3
+
+// Snelheid
+const long maxDel = 2000; // maximum vertraging na elke stap in microseconden; maxDel > minDel >= 500
+const long minDel = 500; // minimum vertraging na elke stap in microseconden; maxDel > minDel >= 500
+const int prec = 100; // bij het draaien van de draaiknop met x-aantal graden neemt de vertraging af/toe met prec aantal microseconden
+
+// variabelen
+int modus = 0;
+int nieuweModus = 0; // nieuweModus is gelijk aan de startmodus; 0 = modus1, 1 = modus2, 2 = modus3 
+int knopStatus[3] = {0, 0, 0}; 
+int knopPin[3] = {knopPin1, knopPin2, knopPin3};
+int draaiHoek[3] = {draaiHoek1, draaiHoek2, draaiHoek3};
+gm gm;
+void setup() {
+  Serial.begin(9600);
+  gm.initPins(dirPin, stepPin, knopPin1, knopPin2, knopPin3);
+  for (int i = 0; i < 3; i++) {
+    bool isKnopIngedrukt = gm.readButton(knopPin[i], knopStatus[i]);
+    if(isKnopIngedrukt) {
+      knopStatus[i] = HIGH;
+    } else {knopStatus[i] = LOW;}
+  }
+}
+
+void loop() {
+  draaiMotor(true); // draai met de klok mee
+  draaiMotor(false); // draai tegen de klok in
+  draaiMotor(false); // draai tegen de klok in
+  draaiMotor(true); // draai met de klok mee
+}
+
+void draaiMotor(bool cw) {
+  modus = nieuweModus;
+  if (draaiHoek[modus] > 90) {return;}
+  for (int i = 0; i <(stepsPerRevolution/360)*draaiHoek[modus]; i++) {
+    long snelheid = gm.calcSpeed(A0, maxDel, minDel, prec);
+    gm.stepmotor(stepPin, dirPin, snelheid, cw);
+    for (int j = 0; j < 3; j++) {
+    bool isKnopIngedrukt = gm.readButton(knopPin[j], knopStatus[j]);
+    if(isKnopIngedrukt) {
+      nieuweModus = j;
+      knopStatus[j] = HIGH;
+    } else {knopStatus[j] = LOW;}
+  }
+  }
+}
